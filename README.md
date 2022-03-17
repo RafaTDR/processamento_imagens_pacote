59350566588150d7c8f76e6e4004b0b3c9cc9c35
RafaelTDR <rafa.eel.tomazoni@gmail.com>
2022-03-16 21:19:38 -0300

    Pacote processamento de imagens

---
+++ README.md
@@ -0,0 +1,28 @@
+# processamento_imagens
+
+Description. 
+The package processamento_imagens is used to:
+
+	- processar imagens 
+	- comparar imagens
+
+## Installation
+
+Use the package manager [pip](https://pip.pypa.io/en/stable/) to install processamento_imagens
+
+```bash
+pip install processamento_imagens
+```
+
+## Usage
+
+```python
+from processamento_imagens.processando import combinacao
+combinacao.find_difference()
+```
+
+## Author
+Rafael TR
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    page_description = f.read()
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="processamento_imagens",
+    version="0.0.1",
+    author="Rafael TR",
+    author_email="Rafa.eel.tomazoni@gmail.com",
+    description="Processamento de imagens",
+    long_description=page_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/RafaTDR/processamento_imagens_pacote.git",
+    packages= find_packages(),
+    install_requires=requirements,
+    python_requires='>=3.8',
+)
